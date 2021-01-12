cc9b7e4b924a0e3ea20acbc569fec85c4ba835bf
Scott O'Neil <scottieoneil@gmail.com>
2021-01-12 14:29:40 -0800

    finish madlib

--- js/scripts.js
+++ js/scripts.js
@@ -78,19 +78,27 @@ $(document).ready(function() {
         $(".color").removeClass().addClass("redbg");
     });
 
-    $(document).ready(function() {
-        $("#formOne").submit(function() {
-            $(".person1").append("blah blah");
-            $(".person2").append("blah blah");
-            $(".animal").append("blah blah");
-            $(".exclamation").append("blah blah");
-            $(".verb").append("blah blah");
-            $(".noun").append("blah blah");
 
-            $("#story").show();
-            event.preventDefault();
 
-        });
+
+    $("#formOne").submit(function(event) {
+        const person1Input = $("input#person1").val();
+        const person2Input = $("input#person2").val();
+        const animalInput = $("input#animal").val();
+        const exclamationInput = $("input#exclamation").val();
+        const verbInput = $("input#verb").val();
+        const nounInput = $("input#noun").val();
+
+        $(".person1").text(person1Input);
+        $(".person2").text(person2Input);
+        $(".animal").text(animalInput);
+        $(".exclamation").text(exclamationInput);
+        $(".verb").text(verbInput);
+        $(".noun").text(nounInput);
+
+        $("#story").show();
+
+        event.preventDefault();
     });
 
 
