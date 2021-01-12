f827f0bf368f682d492366ea679809f6f3a523e3
Scott O'Neil <scottieoneil@gmail.com>
2021-01-12 13:11:59 -0800

    add color change buttons

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,23 @@ $(document).ready(function() {
         $("#batman-showing, #show2, #show3, #showall").slideToggle();
         $("#batman-hidden, #hide2, #hide3, #hideall").slideToggle();
     });
+
+
+
+
+
+    $("button#green").click(function() {
+        $("body").removeClass().addClass("green-background");
+        $(".color").removeClass().addClass("greenbg");
+    });
+
+    $("button#yellow").click(function() {
+        $("body").removeClass().addClass("yellow-background");
+        $(".color").removeClass().addClass("yellbg");
+    });
+
+    $("button#red").click(function() {
+        $("body").removeClass().addClass("red-background");
+        $(".color").removeClass().addClass("redbg");
+    });
 });
